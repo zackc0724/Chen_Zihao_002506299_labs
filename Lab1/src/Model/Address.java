209857db0974793ce7f1354028cd40503f4b4180
zackc0724 <chen.z15@northeastern.edu>
2025-09-14 20:46:46 -0400

    Add Address and Product Class

---
+++ Lab1/src/Model/Address.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 80424
+ */
+class Address {
+    String streetName;
+    String unitNum;
+    String city;
+    String zipCcode;
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getUnitNum() {
+        return unitNum;
+    }
+
+    public void setUnitNum(String unitNum) {
+        this.unitNum = unitNum;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCcode() {
+        return zipCcode;
+    }
+
+    public void setZipCcode(String zipCcode) {
+        this.zipCcode = zipCcode;
+    }
+    
+    
+       
+}
---
+++ Lab1/src/Model/Product.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 80424
+ */
+public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    Address manufactureAddress;
+    Address shippingAddress;
+}
