84f5e2c656aeedc652e6a1f0a8dbf4fc5dfadba4
zackc0724 <chen.z15@northeastern.edu>
2025-10-05 20:17:52 -0400

    Faculty Profile finished

---
+++ ProfileWorkareaExample_fall2025/ProfileWorkareaExample_fall2025/src/Business/Profiles/FacultyProfile.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Profiles;
+
+import Business.Person.Person;
+
+/**
+ *
+ * @author 80424
+ */
+public class FacultyProfile extends Profile {
+    private final Person person;
+
+    public FacultyProfile(Person p) {
+        super(p);
+        this.person = p;
+    }
+
+    @Override
+    public String getRole() {
+        return "Faculty";
+    }
+
+    public boolean isMatch(String id) {
+        return person.getPersonId().equals(id);
+    }
+}
