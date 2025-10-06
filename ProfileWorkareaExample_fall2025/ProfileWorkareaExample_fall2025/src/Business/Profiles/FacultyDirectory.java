c7dd03f9a9cceb0049bd8f0225674a4d1b200baf
zackc0724 <chen.z15@northeastern.edu>
2025-10-05 20:20:30 -0400

    FacultyDirectory finished

---
+++ ProfileWorkareaExample_fall2025/ProfileWorkareaExample_fall2025/src/Business/Profiles/FacultyDirectory.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Profiles;
+
+import Business.Person.Person;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 80424
+ */
+public class FacultyDirectory {
+    
+    private final ArrayList<FacultyProfile> facultyList;
+
+    public FacultyDirectory() {
+        facultyList = new ArrayList<>();
+    }
+
+    public FacultyProfile newFacultyProfile(Person p) {
+        FacultyProfile fp = new FacultyProfile(p);
+        facultyList.add(fp);
+        return fp;
+    }
+
+    public FacultyProfile findFaculty(String id) {
+        for (FacultyProfile fp : facultyList) {
+            if (fp.isMatch(id)) return fp;
+        }
+        return null;
+    }
+
+    public ArrayList<FacultyProfile> getFacultyList() {
+        return facultyList;
+    }
+}
