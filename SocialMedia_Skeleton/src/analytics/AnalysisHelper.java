ec0fad5a8125f3e14d9079bbb6628e05ce83a8a6
zackc0724 <chen.z15@northeastern.edu>
2025-11-09 21:39:49 -0500

    lab 8 skeleton import

---
+++ SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analytics;
+
+/**
+ *
+ * @author harshalneelkamal
+ */
+
+import data.DataStore;
+import java.util.Map;
+import model.Comment;
+
+
+public class AnalysisHelper {
+    //Find Average number of likes per comment.
+    //TODO
+    public void getAverageLikesPerComments() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        int likeNumber = 0;
+        int commentNumber = comments.size();
+        for (Comment c : comments.values()) {
+            likeNumber += c.getLikes();
+        }
+        
+        System.out.println("Average number of likes per comments: " + likeNumber / commentNumber);
+            
+    }
+    
+}
