ba300ac71c3007b626e702d28649503b5a263a39
zackc0724 <chen.z15@northeastern.edu>
2025-11-09 22:48:36 -0500

    Question 5 complete

--- SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -93,7 +93,32 @@ public class AnalysisHelper {
         
         for (int i = 0; i < 5; i++) {
             System.out.println(users.get(i) + ", - Post count: " + postNumbers.get(users.get(i).getId()));
-}
-}
+        }
+    }
+
+    public void getPassiveCommentUsers() {
+    DataStore data = DataStore.getInstance();
+
+    HashMap<Integer, Integer> commentNumbers = new HashMap<Integer, Integer>();
+
+    for (Comment c : data.getComments().values()) {
+        int userId = c.getUserId();
+        if (commentNumbers.containsKey(userId)) {
+            commentNumbers.put(userId, commentNumbers.get(userId) + 1);
+        } else {
+            commentNumbers.put(userId, 1);
+        }
+    }
+
+    ArrayList<User> users = new ArrayList<>(data.getUsers().values());
+
+    Collections.sort(users, new UserMapComparator(commentNumbers));
+
+    System.out.println("Q5 - The following users have the least comments: ");
+    
+    for (int i = 0; i < 5; i++) {
+        System.out.println(users.get(i) + ", - Comment count: " + commentNumbers.get(users.get(i).getId()));
+        }
+    }
 
 }
--- SocialMedia_Skeleton/src/main/SocialMedia_Main.java
+++ SocialMedia_Skeleton/src/main/SocialMedia_Main.java
@@ -98,5 +98,6 @@ public class SocialMedia_Main {
         helper.getMaxLikeCommentPost();
         helper.getPostWithMostComments();
         helper.getPassiveUsers();
+        helper.getPassiveCommentUsers();
     }
 }
