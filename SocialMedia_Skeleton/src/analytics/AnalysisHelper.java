4dc655aabb4da40f898052be14178ddebe983231
zackc0724 <chen.z15@northeastern.edu>
2025-11-09 22:39:45 -0500

    Question 4 finished

--- SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -11,9 +11,13 @@ package analytics;
  */
 
 import data.DataStore;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import model.Comment;
 import model.Post;
+import model.User;
 
 
 public class AnalysisHelper {
@@ -65,5 +69,31 @@ public class AnalysisHelper {
 
         System.out.println("Q3 - post with most comments " + postWithMostComments.getPostId());
     }
+
+    public void getPassiveUsers() {
+    
+        DataStore data = DataStore.getInstance();
+    
+        HashMap<Integer, Integer> postNumbers = new HashMap<Integer, Integer>();
+
+        for (Post p : data.getPosts().values()) {
+            int userId = p.getUserId();
+            if (postNumbers.containsKey(userId)) {
+            postNumbers.put(userId, postNumbers.get(userId) + 1);
+            } else {
+                postNumbers.put(userId, 1);
+            }
+        }
+
+        ArrayList<User> users = new ArrayList<>(data.getUsers().values());
+    
+        Collections.sort(users, new UserMapComparator(postNumbers));
+
+        System.out.println("Q4 - The following users have the least posts: ");
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Post count: " + postNumbers.get(users.get(i).getId()));
+}
+}
 
 }
--- SocialMedia_Skeleton/src/main/SocialMedia_Main.java
+++ SocialMedia_Skeleton/src/main/SocialMedia_Main.java
@@ -97,5 +97,6 @@ public class SocialMedia_Main {
         helper.getAverageLikesPerComments();
         helper.getMaxLikeCommentPost();
         helper.getPostWithMostComments();
+        helper.getPassiveUsers();
     }
 }
